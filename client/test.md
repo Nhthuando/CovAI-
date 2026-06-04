764843e3e59896d9daec333b5b7b59c158d8a6e5
DinhGH <huynhdinh2k52707@gmail.com>
2026-06-04 11:30:37 +0700

    update structure

--- client/test.md
+++
@@ -1,1 +0,0 @@
-Frontend làm sau.
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {login,register} from "../../features/auth/auth.controller.js"
+import { login, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
 
-export default router
+export default router;
