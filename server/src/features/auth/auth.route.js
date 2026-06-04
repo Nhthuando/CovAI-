445902b5f7c66196b53ce678b4e204ff3d941ba3
Nhthuando <thuanhuugl@gmail.com>
2026-06-04 18:18:26 +0700

    commit feat forgot + reset password

--- server/src/features/auth/auth.route.js
+++ server/src/features/auth/auth.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {login,register} from "../../features/auth/auth.controller.js"
+import {forgotPassword,resetPassword} from "../../features/auth/forgotPassword.controller.js"
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword/:token", resetPassword);
+
 
 export default router
