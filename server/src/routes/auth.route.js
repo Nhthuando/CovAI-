93e6e745b55439e1e4a825ac99a78d7bcbb73b2b
DinhGH <huynhdinh2k52707@gmail.com>
2026-06-11 21:07:40 +0700

    update github access

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,16 @@
 import express from "express";
-import {login,register, oAuthGithub} from "../controllers/auth.controller.js"
-import {forgotPassword,resetPassword} from "../controllers/forgotPassword.controller.js"
+import {
+  login,
+  register,
+  oAuthGithub,
+  githubOAuthAccess,
+  getGithubRepositories,
+} from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import {
+  forgotPassword,
+  resetPassword,
+} from "../controllers/forgotPassword.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +19,7 @@ router.post("/register", register);
 router.post("/forgotPassword", forgotPassword);
 router.post("/resetPassword/:token", resetPassword);
 router.get("/github/callback", oAuthGithub);
+router.post("/github/access", authMiddleware, githubOAuthAccess);
+router.get("/github/repositories", authMiddleware, getGithubRepositories);
 
 export default router;
