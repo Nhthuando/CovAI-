9a2fe163a32d8a86e2fb18cfcc3407e26e77654a
DinhGH <huynhdinh2k52707@gmail.com>
2026-06-23 09:36:46 +0700

    remove backup folder

--- backup_code/routes/aiSuggestion.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as aiSuggestionController from '../controllers/aiSuggestion.controller.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
-
-const router = express.Router();
-
-router.get('/', authMiddleware, aiSuggestionController.getAiSuggestions);
-
-export default router;
